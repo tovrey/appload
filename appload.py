d114aa799b89c8d7955a4bcf189632eb2e389d5b
Andrey Motov <tovrey@gmail.com>
2017-10-04 09:17:02 +0300

    Functions download_track, gen_apple_pl, main Downloads tracks into playlist folder

--- appload.py
+++ appload.py
@@ -1,6 +1,7 @@
 from requests import request
-from json import dumps
 from sys import argv, exit
+from os import mkdir
+from os.path import exists
 from lxml.html import fromstring
 
 
@@ -11,7 +12,8 @@ if len(argv) < 2:
 else:
     PLID = argv[1]
 
-APPLE_ROOT_XPATH = ".//*[@id='tracks']/table"
+APPLE_TRACKS_XPATH = ".//*[@id='tracks']/table"
+APPLE_PL_NAME_XPATH = ".//*[@id='playlistHero']/table/tr/td[2]/div[1]/a/text()"
 APPLE_LINK = "http://tools.applemusic.com/embed/v1/playlist/" + PLID
 
 HEADERS={"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)" +
@@ -27,15 +29,47 @@ HEADERS={"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, 
 
 APPLE_CONTENT = request("GET", APPLE_LINK, headers=HEADERS).content
 APPLE_TREE = fromstring(APPLE_CONTENT)
-TRACKS = APPLE_TREE.xpath(APPLE_ROOT_XPATH)
-PLAYLIST = []
-for track, num in zip(TRACKS, range(1, len(TRACKS) + 1)):
-    new_track = {}
-    new_track['pl-num'] = str(num)
-    new_track['data-id'] = track.get('data-id')
-    new_track['title'] = track.xpath('tr/td[2]/div[1]/div[1]/text()')[0].strip()
-    new_track['artist'] = track.xpath('tr/td[2]/div[2]/text()')[0].strip()
-    new_track['audio'] = track.xpath('tr/td[1]/div/div')[0].get('data-url')
-    PLAYLIST.append(new_track)
 
-print(dumps(PLAYLIST, indent=4, ensure_ascii=False))
+    
+def gen_apple_pl(plid):
+    playlist = {}
+    playlist['id'] = plid
+    playlist['name'] = str(APPLE_TREE.xpath(APPLE_PL_NAME_XPATH)[0].strip())
+    playlist['tracks'] = []
+    tracks = APPLE_TREE.xpath(APPLE_TRACKS_XPATH)
+    for track, num in zip(tracks, range(1, len(tracks) + 1)):
+        new_track = {}
+        new_track['pl-num'] = str(num).rjust(len(str(len(tracks))), '0')
+        new_track['data-id'] = track.get('data-id')
+        new_track['title'] = track.xpath('tr/td[2]/div[1]/div[1]/text()')[0].strip()
+        new_track['artist'] = track.xpath('tr/td[2]/div[2]/text()')[0].strip()
+        new_track['audio-url'] = track.xpath('tr/td[1]/div/div')[0].get('data-url')
+        playlist['tracks'].append(new_track)
+    return playlist
+
+
+def download_track(link, number, artist, title, dir_name):
+    if not exists(dir_name):
+        mkdir(dir_name)
+    file_name = number + ". " + artist + " - " + title + ".mp3"
+    bad_symbols = ["/", "\\"]
+    for symbol in bad_symbols:
+        if symbol in file_name:
+            file_name = file_name.replace(s, "_")
+    full_path = dir_name + "/" + file_name
+    with open(full_path, 'wb') as f:                                                                               
+        f.write(request("GET", link, headers=HEADERS).content)
+    return True
+
+
+def main():
+    """docstring for main"""
+    playlist = gen_apple_pl(PLID)
+    for track in playlist['tracks']:
+        download_track(track['audio-url'], track['pl-num'], track['artist'], track['title'],
+                playlist['name'])
+        print('Track "' + track['title'] + '" is downloaded')
+    
+
+if __name__ == '__main__':
+    main()
